51b65613a91629e58daa4364a143283a691832c8
Reza Darzi <darzi.reza@gmail.com>
2025-11-13 01:29:21 +0330

    feat: Add Video model in admin site

--- filemanager/video/admin.py
+++ filemanager/video/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from unfold.admin import ModelAdmin
+from .models import VideoModel
 
-# Register your models here.
+@admin.register(VideoModel)
+class VideoAdmin(ModelAdmin):
+    # change_form_template = "admin/upload_video.html"
+    list_display = ("title", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("title", "created_at", "updated_at")
+    list_per_page = 25
+
+    def get_fields(self, request, obj = None):
+        if obj is None:
+            return ("title", "video")
+        else:
+            return (
+                "title",
+                "video",
+                "audio",
+                "transcript_text",
+                "transcript_jsonl"
+            )
+
+    def video_icon(self, obj):
+        return format_html("ERE" if obj.video else "ttt")
