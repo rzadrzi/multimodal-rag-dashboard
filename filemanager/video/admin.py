d9bcb7e7bbbb5bd0aeaa1421602e452318beab12
Reza Darzi <darzi.reza@gmail.com>
2025-11-13 01:58:30 +0330

    feat: Add in admin site get_urls method

--- filemanager/video/admin.py
+++ filemanager/video/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.urls import URLPattern
 from django.utils.html import format_html
 from unfold.admin import ModelAdmin
 from .models import VideoModel
+
+from django.template.response import TemplateResponse
+from django.urls import path
 
 @admin.register(VideoModel)
 class VideoAdmin(ModelAdmin):
@@ -10,6 +14,10 @@ class VideoAdmin(ModelAdmin):
     list_filter = ("created_at", "updated_at")
     search_fields = ("title", "created_at", "updated_at")
     list_per_page = 25
+
+    def get_urls(self) -> list[URLPattern]:
+        urls = super().get_urls()
+        my_urls = [path("my_view/", self.admin_site.admin_view)]
 
     def get_fields(self, request, obj = None):
         if obj is None:
