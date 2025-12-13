93a258d970893ad89d9887ae80c25dbaecb35bb0
Reza Darzi <darzi.reza@gmail.com>
2025-12-13 19:57:32 +0330

    refact: Use Pylint

--- app/filemanager/video/models.py
+++ app/filemanager/video/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 # Create your models here.
 class VideoModel(models.Model):
     """
@@ -8,19 +9,17 @@ class VideoModel(models.Model):
     and with ASR Models and Whisper Models Return back Audios and transcripts files.
     All these in background
     """
-    LANG = {
-        'FA':'fa',
-        'EN':'en'
-    }
+
+    LANG = {"FA": "fa", "EN": "en"}
 
     class Status(models.TextChoices):
-        UPLOAD ="upload"
+        UPLOAD = "upload"
         AUDIO = "audio"
         TRANSCRIPT = "transcript"
 
     class Meta:
-        verbose_name = 'Upload Video'
-        verbose_name_plural = 'Videos'
+        verbose_name = "Upload Video"
+        verbose_name_plural = "Videos"
 
     title = models.CharField(max_length=255, null=True, blank=True)
     slug = models.SlugField(max_length=255, null=True, blank=True, unique=True)
@@ -32,15 +31,12 @@ class VideoModel(models.Model):
     transcript_jsonl = models.FileField(
         upload_to="upload/transcripts", blank=True, null=True
     )
-    transcript_srt = models.FileField(
-        upload_to="upload/srt", blank=True, null=True
-    )
+    transcript_srt = models.FileField(upload_to="upload/srt", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
